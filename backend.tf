9d20e85911843f0b06bf311bd08f6d304727d407
Thailik <thai@Thailiks-MacBook-Pro.local>
2026-05-01 10:27:30 -0400

    FEAT:scale up dev env

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "gha-project-bucket-42026"
-    key          = "terraform/state/main/terraform.tfstate"
-    region       = "us-east-1" 
-    use_lockfile = true        
-    encrypt      = true
-  }
-}
